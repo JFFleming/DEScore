257f412e0d0d60b6eddc8274733135b4fc68d2ff
JFFleming <84772413+JFFleming@users.noreply.github.com>
2025-12-17 20:04:31 +0900

    Create Noisemaker.pl
    Noisemaker.pl is a translation of the Ruby script to perl

---
+++ Noisemaker.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# Version and startup message
+my $version = 1.1;
+print "Noisemaker Version $version, October 14th 2025 written by James Fleming & Torsten H. Struck\n";
+
+# Amino acid lookup table
+my %aminoAcids = (
+    1  => 'A',  2  => 'R',  3  => 'N',  4  => 'D',  5  => 'C',
+    6  => 'Q',  7  => 'E',  8  => 'G',  9  => 'H',  10 => 'I',
+    11 => 'L',  12 => 'K',  13 => 'M',  14 => 'F',  15 => 'P',
+    16 => 'S',  17 => 'T',  18 => 'W',  19 => 'Y',  20 => 'V'
+);
+
+# Command-line arguments
+my $inputNumber = $ARGV[0];
+my $inputLength = $ARGV[1];
+my $simNumber   = $ARGV[2];
+
+# Random seed handling
+my $seedNumber;
+if (defined $ARGV[3]) {
+    $seedNumber = $ARGV[3];
+    srand($seedNumber);
+} else {
+    $seedNumber = time();
+    srand($seedNumber);
+    print "$seedNumber\n";
+}
+
+# Output directory
+my $output_dir = "Noisemaker_Output_$seedNumber";
+if (-d $output_dir) {
+    die "Warning: output directory '$output_dir' already exists. Aborting to prevent overwrite.\n";
+} else {
+    mkdir $output_dir or die "Failed to create directory '$output_dir': $!\n";
+}
+
+# Write seed file
+open(my $seed_fh, '>', "$output_dir/NoiseMakerSeed.txt") or die $!;
+print $seed_fh "$seedNumber\n";
+close $seed_fh;
+
+# Convert inputs to integers
+my $seqNumber = int($inputNumber);
+my $seqLength = int($inputLength);
+my $sims      = int($simNumber);
+
+# Simulation loop
+for (my $sim = 0; $sim < $sims; $sim++) {
+
+    my $fileNumber = $sim + 1;
+    my $fName = "$output_dir/$fileNumber.Noise.fas";
+
+    open(my $fh, '>>', $fName) or die $!;
+
+    for (my $i = 0; $i < $seqNumber; $i++) {
+
+        my $seq = '';
+
+        for (my $j = 0; $j < $seqLength; $j++) {
+            my $rando = int(rand(20)) + 1;
+            $seq .= $aminoAcids{$rando};
+        }
+
+        my $header = ">t" . ($i + 1) . "\n";
+        print $fh $header, $seq, "\n";
+#        print $header, $seq, "\n";
+    }
+
+    close $fh;
+}
